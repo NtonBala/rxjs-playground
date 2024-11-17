2d78e2bf0be6dbbb772e70e5c3859d599f6130e7
Anton Balashov <balashov.anton.1983@gmail.com>
2024-11-17 18:15:21 +0200

    1.3. sumulate HTTP request with observable

--- src/1/3.ts
+++ src/1/3.ts
@@ -1,7 +1,20 @@
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
-const name$ = of('Alice', 'Ben', 'Charlie');
+// * Creating & running the observable
+// const name$ = of('Alice', 'Ben', 'Charlie');
 
-name$.subscribe((value) => {
-  console.log(value);
-});
+// name$.subscribe((value) => {
+//   console.log(value);
+// });
+
+// * Simulate HTTP request with observable
+const storeDataOnServer = (data: string) => {
+  return new Observable((subscriber) => {
+    setTimeout(() => {
+      subscriber.next(`Saved successfully: ${data}`);
+      subscriber.complete();
+    }, 5000);
+  });
+};
+
+storeDataOnServer('Some value').subscribe((value) => console.log(value));
