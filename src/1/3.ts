107bd0574c2faa7a0958ccc7025fe9862f5f8edf
Anton Balashov <balashov.anton.1983@gmail.com>
2024-11-17 19:45:00 +0200

    1.3. sumulate HTTP request error with observable

--- src/1/3.ts
+++ src/1/3.ts
@@ -8,13 +8,27 @@ import { Observable, of } from 'rxjs';
 // });
 
 // * Simulate HTTP request with observable
-const storeDataOnServer = (data: string) => {
+// const storeDataOnServer = (data: string) => {
+//   return new Observable((subscriber) => {
+//     setTimeout(() => {
+//       subscriber.next(`Saved successfully: ${data}`);
+//       subscriber.complete();
+//     }, 5000);
+//   });
+// };
+
+// storeDataOnServer('Some value').subscribe((value) => console.log(value));
+
+// * Simulate HTTP request error with observable
+const storeDataOnServerError = (data: string): Observable<string> => {
   return new Observable((subscriber) => {
     setTimeout(() => {
-      subscriber.next(`Saved successfully: ${data}`);
-      subscriber.complete();
+      subscriber.error(new Error('Failure!'));
     }, 5000);
   });
 };
 
-storeDataOnServer('Some value').subscribe((value) => console.log(value));
+storeDataOnServerError('Some value').subscribe({
+  next: (value) => console.log(value),
+  error: (err) => console.log(`Error when saving: ${err.message}`),
+});
